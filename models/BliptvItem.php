349778882b1f7a4afcddb995df1054ff310182c8
John Hobbs <john@velvetcache.org>
2010-02-18 14:51:51 -0600

    Patch from http://github.com/jmhobbs/storytlr - Two fixes for blip.tv. Auto-center slides and remove HTML em element from titles.

--- models/BliptvItem.php
+++ models/BliptvItem.php
@@ -14,24 +14,24 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *  
+ *
  */
 class BliptvItem extends SourceItem {
 
 	protected $_prefix = 'bliptv';
-	
+
 	public function toArray () {
 		return $this->_data;
 	}
-	
+
 	public function getTitle () {
 		$duration = intval( $this->_data['length'] );
-		
+
 		$composite = "";
-		
+
 		$hours = floor( $duration / 3600 );
 		$duration = $duration - ( $hours * 3600 );
-		
+
 		if( $hours == 1 )
 			$composite .= "1 Hour ";
 		else if (  $hours > 1 )
@@ -39,37 +39,37 @@ class BliptvItem extends SourceItem {
 
 		$minutes = floor( $duration / 60 );
 		$seconds = $duration - ( $minutes * 60 );
-		
+
 		$composite .= sprintf( "%02d:%02d", $minutes, $seconds );
-		
-		return $this->_data['title'] . " <em>($composite)</em>";
+
+		return $this->_data['title'] . " ($composite)";
 	}
-	
+
 	public function getEmbed () {
 		return $this->_data['embed'];
 	}
-	
+
 	public function getLink () {
 		return $this->_data['link'];
 	}
-	
+
 	public function getDescription () {
 		return $this->_data['content'];
 	}
-	
+
 	public function getType () {
 		return SourceItem::VIDEO_TYPE;
 	}
-	
+
 	public function getImageUrl ( $size = ImageItem::SIZE_THUMBNAIL ) {
 		return $this->_data['thumbnail'];
 	}
-	
+
 	public function getBackup() {
 		$item = array();
 		$item['Link']				= $this->getLink();
 		$item['Title']				= $this->_data['title'];
-		$item['Date']				= $this->_data['published'];		
+		$item['Date']				= $this->_data['published'];
 		return $item;
 	}
 }
